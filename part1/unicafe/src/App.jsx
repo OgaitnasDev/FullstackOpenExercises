8444c307bda032c47207cc14fc3ffb19b8b9d801
OgaitnasDev <ogaitnas1_@hotmail.com>
2025-04-16 17:31:07 -0600

    1.9 unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,13 @@ const Button = ({onClick, text}) => {
 }
 
 const Statistics = ({good, neutral, bad, all, average, positive}) => {
+	if(all === 0) return(
+		<div>
+			<h1>statistics</h1>
+			<p>No feedback given</p>
+		</div>
+	)
+
 	return(
 		<div>
 			<h1>statistics</h1>
