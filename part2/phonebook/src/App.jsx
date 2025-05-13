459ce0f85e9d961952649b808d1409fbd505e090
OgaitnasDev <ogaitnas1_@hotmail.com>
2025-05-12 23:30:23 -0600

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'John Doe' }
+    { name: 'Nishizumi Miho', number: '0000000000' },
+    { name: 'Takebe Saori', number: '1111111111'},
+    { name: 'Isuzu Hana', number:  '2222222222'},
+    { name: 'Akiyama Yukari', number: '3333333333' },
+    { name: 'Reizei Mako', number: '4444444444' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const personsToShow = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
 
   const addPerson = (event) => {
     event.preventDefault()
-    
+
     if(!persons.map(p => p.name).includes(newName)){
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson))
     } 
@@ -20,18 +29,32 @@ const App = () => {
       console.log("Person already exists")
     }
     setNewName('')
+    setNewNumber('')
   }
 
 	const handleOnChange = (event) => {
 		setNewName(event.target.value)
 	}
+  
+  const handleNumberOnChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterOnChange = (event) =>{
+    setFilter(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter by name: <input value={filter} onChange={handleFilterOnChange}></input>
       <form  onSubmit={addPerson}>
+        <div><h3>Add new</h3></div>
         <div>
           name: <input value={newName} onChange={handleOnChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberOnChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -40,7 +63,7 @@ const App = () => {
       <h2>Numbers</h2>
 			<table>
 				<tbody>
-					{persons.map( (p) => <tr key={p.name}><td>{p.name}</td></tr>)}
+					{personsToShow.map( (p) => <tr key={p.name}><td>{p.name}</td><td>{p.number}</td></tr>)}
 				</tbody>
 			</table>
       
