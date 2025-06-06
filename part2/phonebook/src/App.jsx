8583ddcfd1cef746112efc7e7f6a8d6f2e47222d
OgaitnasDev <ogaitnas1_@hotmail.com>
2025-06-05 18:55:24 -0600

    2.12: The Phonebook step 7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -25,11 +25,14 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(newPerson))
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => setPersons(persons.concat(response.data)));
+      
     } 
     else {
-      alert(`${newName} is already added to phonebook`)
-      console.log("Person already exists")
+      alert(`${newName} is already added to phonebook`);
+      console.log("Person already exists");
     }
     setNewName('')
     setNewNumber('')
